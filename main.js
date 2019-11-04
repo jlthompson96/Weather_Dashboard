ff82845e9a6de75faf02a1ee9993e36490d90457
jlthompson96 <joeythompson1014@gmail.com>
2019-11-04 10:43:30 -0500

    added link to DarkSky and removed API key

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var request = new XMLHttpRequest();
-request.open('GET','https://api.darksky.net/forecast/071f00969655c3ebc678622c4fc0d1f0/35.2271,-80.8431');
+//request.open('GET','DarkSkyAPI Key');
 
 request.onload = function() {
     var weatherData = JSON.parse(request.responseText);
