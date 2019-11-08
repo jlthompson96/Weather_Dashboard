4bd054929cfbdd91e6286b1984c80ca276ac70ba
jlthompson96 <joeythompson1014@gmail.com>
2019-11-07 21:31:47 -0500

    testing api key.

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var request = new XMLHttpRequest();
-//request.open('GET','DarkSkyAPI Key');
+request.open('GET','https://api.darksky.net/forecast/071f00969655c3ebc678622c4fc0d1f0/37.8267,-122.4233');
 
 request.onload = function() {
     var weatherData = JSON.parse(request.responseText);
-    console.log(weatherData[0]);
+    document.getElementById("temp").innerHTML = myObj.name;
 };
 
 request.send();
