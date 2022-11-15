c916bd9a33ed95486064f1d71b182559f361e4b6
Joey Thompson <joeythompson1014@gmail.com>
2022-11-15 08:57:14 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '71daac2480mshd55aa58942738fap15f7bdjsn0e359670f9cd',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+		'X-RapidAPI-Key': 'API_KEY',
+		'X-RapidAPI-Host': 'API_HOST'
 	}
 };
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
-export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-export const WEATHER_API_KEY = '65572ebc1008728ad26012a34385268b'
+export const WEATHER_API_URL = 'API_URL';
+export const WEATHER_API_KEY = 'API_KEY'
