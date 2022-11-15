930d7c349cd1de6974363dfc69b5ae9b82ebd0dc
Joey Thompson <joeythompson1014@gmail.com>
2022-11-15 08:54:46 -0500

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '71daac2480mshd55aa58942738fap15f7bdjsn0e359670f9cd',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '65572ebc1008728ad26012a34385268b'
