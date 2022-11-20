72924a1698ead7e073a15d88adcf26321b877ef8
Joey Thompson <joeythompson1014@gmail.com>
2022-11-20 12:47:40 -0500

    Added glassmorphism styling

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -49,7 +49,7 @@ const Forecast = ({ data }) => {
                             </div>
                         </AccordionItemPanel>
                     </AccordionItem>
-                ))};
+                ))}
             </Accordion>
         </>
     );
